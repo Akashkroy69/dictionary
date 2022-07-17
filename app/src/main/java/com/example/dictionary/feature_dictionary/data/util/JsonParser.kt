d6e49856a8fb57e318595ceabd90e462865ff236
Akashkroy69 <Akashkroy@hotmail.com>
2022-07-17 18:39:00 +0530

    Interface WordInfoRepository has been added

--- app/src/main/java/com/example/dictionary/feature_dictionary/data/util/JsonParser.kt
+++ app/src/main/java/com/example/dictionary/feature_dictionary/data/util/JsonParser.kt
@@ -2,7 +2,7 @@ package com.example.dictionary.feature_dictionary.data.util
 
 import java.lang.reflect.Type
 
-
+//This interface will contain two functions. one to get an object from a json string and the other to parse an object to a json string
 interface JsonParser {
 
     fun <T> fromJson(json: String, type: Type): T?
