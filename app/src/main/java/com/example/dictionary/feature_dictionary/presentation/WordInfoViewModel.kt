d0ecffd56e9e4026a7ef77d7b6ded2845c877027
Akashkroy69 <Akashkroy@hotmail.com>
2022-07-23 20:41:32 +0530

    ERROR in WordInforViewModel.kt: To be fixed

--- app/src/main/java/com/example/dictionary/feature_dictionary/presentation/WordInfoViewModel.kt
+++ app/src/main/java/com/example/dictionary/feature_dictionary/presentation/WordInfoViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.dictionary.feature_dictionary.presentation
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.dictionary.feature_dictionary.domain.use_case.GetWordInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,4 +13,8 @@ import javax.inject.Inject
 class WordInfoViewModel @Inject constructor(
     private val getWordInfo: GetWordInfo
 ) : ViewModel() {
+    private val _searchQuery = mutableStateOf("")
+    val searchQuery: State<String> = _searchQuery
+
+    private val _state = mutableStateOf<WordInfoState>()
 }
